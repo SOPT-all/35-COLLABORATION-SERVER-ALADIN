632d54b4a2eb258426fc5ea3327b086aefeb2506
Parkjyun <jaeyun0507@naver.com>
2024-11-18 23:36:47 +0900

    feat: User entity 생성

---
+++ src/main/java/com/aladin/aladinserver/entity/User.java
@@ -0,0 +1,24 @@
+package com.aladin.aladinserver.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    protected User() {
+    }
+}
