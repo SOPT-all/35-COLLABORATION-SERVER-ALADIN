2e9caddf51ffa9df8bb7214b3e6b55870cc3671a
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-11-18 23:41:59 +0900

    [feat] jpa entity 생성 (#2)
    * feat: Book entity 생성
    
    * feat: Category entity 생성
    
    * feat: User entity 생성

---
+++ src/main/java/com/aladin/aladinserver/entity/Book.java
@@ -0,0 +1,43 @@
+package com.aladin.aladinserver.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "book")
+public class Book {
+
+    @Id
+    @Column(name = "book_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "price", nullable = false)
+    private Integer price;
+
+    @Column(name = "vote_count", nullable = false)
+    private Integer voteCount;
+
+    @Column(name = "image_url", nullable = false)
+    private String imageUrl;
+
+    @Column(name = "discount", nullable = false)
+    private Double discount;
+
+    protected Book() {
+    }
+}
