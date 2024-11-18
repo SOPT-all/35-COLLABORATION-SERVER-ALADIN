8ceaaa22fa2fd8b8c2222698bb943dc8e7b15964
Parkjyun <jaeyun0507@naver.com>
2024-11-18 23:36:43 +0900

    feat: Category entity 생성

---
+++ src/main/java/com/aladin/aladinserver/entity/Category.java
@@ -0,0 +1,27 @@
+package com.aladin.aladinserver.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "parent_id", nullable = true)
+    private Long parentId;
+
+    protected Category() {
+    }
+}
