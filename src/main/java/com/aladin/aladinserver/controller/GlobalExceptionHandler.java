5d52e14a4cecf4a452c63bf1b5aa41caf7069900
Parkjyun <jaeyun0507@naver.com>
2024-11-22 21:53:48 +0900

    feat: exceptionhandler 생성

---
+++ src/main/java/com/aladin/aladinserver/controller/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.aladin.aladinserver.controller;
+
+import com.aladin.aladinserver.common.CustomException;
+import com.aladin.aladinserver.controller.response.AladinResponse;
+import com.aladin.aladinserver.common.ErrorCode;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public AladinResponse<Void> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        return AladinResponse.fail(ErrorCode.METHOD_NOT_ALLOWED);
+    }
+
+    @ExceptionHandler(CustomException.class)
+    public AladinResponse<Void> handleCustomException(CustomException e) {
+        return AladinResponse.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public AladinResponse<Void> handleException(Exception e) {
+        return AladinResponse.fail(ErrorCode.BAD_REQUEST);
+    }
+}
