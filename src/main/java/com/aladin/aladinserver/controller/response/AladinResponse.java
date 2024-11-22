6908d2b2df463b652421e0dcbb0891769a49ee9f
Parkjyun <98092394+Parkjyun@users.noreply.github.com>
2024-11-22 21:58:39 +0900

    [feat] 공통 응답구조, exception handler 추가 (#4)
    * feat: 응답 코드 생성
    
    * feat: 공통 응답 생성
    
    * feat: exceptionhandler 생성
    
    * feat: 의존성 추가
    
    * chore: 사용하지 않는 설정 파일 삭제

---
+++ src/main/java/com/aladin/aladinserver/controller/response/AladinResponse.java
@@ -0,0 +1,50 @@
+package com.aladin.aladinserver.controller.response;
+
+
+import com.aladin.aladinserver.common.ErrorCode;
+import com.aladin.aladinserver.common.SuccessCode;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class AladinResponse<T> {
+
+    private final int code;
+    private final String message;
+    @JsonInclude(value = JsonInclude.Include.NON_NULL)//null이 아닌것만(nonull)을 응답에 포함하겠다
+    private T data;
+
+    public AladinResponse(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public AladinResponse(int code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> AladinResponse<T> success(SuccessCode success) {
+        return new AladinResponse<>(success.getHttpStatus().value(), success.getMessage());
+    }
+
+
+    public static <T> AladinResponse<T> success(SuccessCode success, T data) {
+        return new AladinResponse<>(success.getHttpStatus().value(), success.getMessage(), data);
+    }
+
+    public static <T> AladinResponse<T> fail(ErrorCode error) {
+        return new AladinResponse<>(error.getHttpStatus().value(), error.getMessage());
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
